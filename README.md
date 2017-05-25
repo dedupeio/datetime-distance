3f2c504388e16e327c6a0172577665e9d9452217
Jean Cochrane <jean@jeancochrane.com>
2017-05-25 14:22:49 -0500

    First commit - sketch out repo architecture

---
+++ README.md
@@ -0,0 +1,19 @@
+# datetime-distance
+
+Python library to compare string distances between dates, timestamps, or datetime objects.
+
+### Development
+
+Install dev requirements with pip:
+
+```
+pip install dev-requirements.txt
+```
+
+### Testing
+
+```
+pytest tests
+```
+
+All tests must pass.
---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    name='datetime-distance',
+    url='https://github.com/dedupeio/datetime-distance',
+    version='0.0.1',
+    description='Compare string distances between dates, timestamps, or datetime objects.',
+    packages=['datetime-distance'],
+    install_requires=['python-dateutil']
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+)
